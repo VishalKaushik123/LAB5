a3136e6c73b7584e9e24cd450cc864376fd9b784
VishalKaushik123 <84299956+VishalKaushik123@users.noreply.github.com>
2021-07-02 11:45:07 -0500

    Update LAB5.cpp

--- src/LAB5.cpp
+++ src/LAB5.cpp
@@ -10,27 +10,27 @@
  * Author:
  * Date:
  */
-void setup();
-void loop();
-#line 7 "c:/Users/Vishal/Documents/CTDProjects/LAB5/src/LAB5.ino"
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 uint16_t value; 
-uint16_t maxValue;
-uint16_t minValue;
+uint16_t maxValue = 0;
+uint16_t minValue = 4095;
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-  pinMode(D8, OUTPUT);
-  pinMode(A5, INPUT);   
-  maxValue = max(maxValue,value);
-  minValue = min(minValue, value);
+  pinMode(D8, OUTPUT); //sets D8 as output
+  pinMode(A5, INPUT);  //sets A5 as input
   
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  value = analogRead(A5);
-  analogWrite(D8,map(value, minValue, maxValue, 0, 255));
+  value = analogRead(A5); //initalizes value with the input from A5(the photo resistor)
+
+  maxValue = max(maxValue,value); //sets maxValue to the highest value inputed, bright room
+  minValue = min(minValue, value); //sets minValue to the highest value inputed, dark room
+
+  analogWrite(D8,map(value, minValue, maxValue, 0, 255)); //converts the 0-4095 number to range between 0-255, and turns led on
+  
 }
